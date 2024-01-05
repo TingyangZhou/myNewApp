5fd3eda095e325961fd462dcf8500fa4c0dd441d
TinaZhou <zty_312@163.com>
2024-01-04 18:30:12 -0600

    version-2

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def readDetails(filepath):
-    with open(filepath, 'r') as f:
-        return [line for line in f]
+# def readDetails(filepath):
+#     with open(filepath, 'r') as f:
+#         return [line for line in f]
 
 # Make a homepage
 @app.route('/')
@@ -13,18 +13,14 @@ def homepage():
 
 @app.route('/hello/<name>')
 def hello(name):
-# return render_template('name.html', Sname = name) 
+# return render_template('name.html', Sname = name)
 # The first argument is the name of the template file. In this case, it's 'name.html'. This file should be located in the templates folder.
-# The second argument is a keyword argument that passes data to the template.  
+# The second argument is a keyword argument that passes data to the template.
     listOfNames = [name, "Yoyo", "Yennifer"]
-    return render_template('name.html', Sname = name,  nameList = listOfNames) 
+    return render_template('name.html', Sname = name,  nameList = listOfNames)
 
-@app.route('/form', methods=['GET', 'POST'])
-def formDemo(name = None):
-    if request.method == 'POST':
-        name=request.form['Sname'] # assign the name retrieved from form to name
-    return render_template('form.html', name=name)
+
 
 
 if __name__ =="__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
